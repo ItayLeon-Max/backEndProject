e6d8af456590b36767ae3e63caf97aff1e70364e
Itay Leon <itayleon81@walla.com>
2025-03-30 07:45:53 +0300

    done1

---
+++ gmailServer/backend/src/app.ts
@@ -0,0 +1,46 @@
+import express, { json } from "express"
+import config from 'config'
+import sequelize from "./db/sequelize"
+import errorLogger from "./middlewares/error/error-logger"
+import errorResponder from "./middlewares/error/error-responder"
+import notFound from "./middlewares/not-found"
+import cors from 'cors'
+import developmentGrouprouter from "./routers/developmentGroup/developmentGroup"
+import meetingRouter from "./routers/meetings"
+import reminderRouter from "./routers/reminder"
+import authRouter from "./routers/auth/auth"
+import taskRouter from "./routers/task"
+
+const port = config.get<string>('app.port')
+const name = config.get<string>('app.name')
+const force = config.get<boolean>('sequelize.sync.force')
+
+const app = express();
+
+export async function start() {
+    await sequelize.sync({ force })
+
+    // middlewares
+    app.use(cors()) // allow any client to use this server
+
+    app.use(json()) // a middleware to extract the post/put/patch data and save it to the request object in case the content type of the request is application/json
+
+    // routers
+    app.use('/developmentGroup', developmentGrouprouter)
+    app.use('/meeting', meetingRouter)
+    app.use('/reminder', reminderRouter)
+    app.use('/auth', authRouter)
+    app.use('/task', taskRouter)
+    
+
+    // special notFound middleware
+    app.use(notFound)
+
+    // error middleware
+    app.use(errorLogger)
+    app.use(errorResponder)
+
+    // app.listen(port, () => console.log(`${name} started on port ${port}...`))
+}
+
+export default app
