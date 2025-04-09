d0594ec801c7fd04691b63946123b04a8a617d79
Itay Leon <itayleon81@walla.com>
2025-04-09 22:08:12 +0300

    feat: add sent email functionality and sync process
    - Added SentEmail model to handle sent emails in the database.
    - Implemented syncSentEmails function to retrieve and store sent emails from Gmail.
    - Created controller for deleting sent emails with proper error handling.
    - Introduced sentEmailRouter for handling sent email routes.
    - Updated app initialization to sync all users' inboxes on startup.
    - Enhanced email body storage to support longer text.
    - Integrated mailparser library for potential future email parsing needs.

--- Server With SQL/gmailServer/backend/src/db/sequelize.ts
+++ Server With SQL/gmailServer/backend/src/db/sequelize.ts
@@ -7,13 +7,14 @@ import EmailLabel from "../models/emailLabel";
 import Draft from "../models/draft";
 import SpamEmail from "../models/spamEmail";
 import GoogleCredential from "../models/googleCredential";
+import SentEmail from "../models/sentEmail";
 
 
 const logging = config.get<boolean>('sequelize.logging') ? console.log : false
 
 const sequelize = new Sequelize({
     // [ add ALL model classes you created to the array ]:
-    models: [  User, Email, Label, EmailLabel, Draft, SpamEmail, GoogleCredential  ],
+    models: [  User, Email, Label, EmailLabel, Draft, SpamEmail, GoogleCredential, SentEmail  ],
     dialect: 'mysql',
     ...config.get('db'),
     logging,
