2100031070167e902a077bf8fa123ea2aceac925
Itay Leon <itayleon81@walla.com>
2025-11-30 09:50:19 +0200

    chore: remove unused models from Sequelize initialization

--- Server With SQL/BankServer/backend/src/db/sequelize.ts
+++ Server With SQL/BankServer/backend/src/db/sequelize.ts
@@ -1,15 +1,14 @@
 import { Sequelize } from "sequelize-typescript";
 import config from 'config'
 import User from "../models/user";
-import BankAccount from "../models/bankAccount";
-import Transaction from "../models/transaction";
+
 
 
 const logging = config.get<boolean>('sequelize.logging') ? console.log : false
 
 const sequelize = new Sequelize({
     // [ add ALL model classes you created to the array ]:
-    models: [ User , BankAccount, Transaction   ],
+    models: [ User ],
     dialect: 'mysql',
     ...config.get('db'),
     logging,
