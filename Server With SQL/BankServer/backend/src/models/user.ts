e8dd1c9f251e4e2834a52680b4de10d25ba39523
Itay Leon <itayleon81@walla.com>
2025-11-30 09:57:51 +0200

    refactor: rename User model to BankAccount and update table structure

--- Server With SQL/BankServer/backend/src/models/user.ts
+++ Server With SQL/BankServer/backend/src/models/user.ts
@@ -1,43 +1,36 @@
 import {
-    AllowNull,
-    Column,
-    DataType,
-    Default,
-    HasMany,
-    Model,
-    PrimaryKey,
-    Table,
+  AllowNull,
+  BelongsTo,
+  Column,
+  DataType,
+  Default,
+  ForeignKey,
+  Model,
+  PrimaryKey,
+  Table,
 } from "sequelize-typescript";
+import User from "./user";
 
 @Table({
-    tableName: "users",
-    underscored: true,
+  tableName: "bank_accounts",
+  underscored: true,
 })
-export default class User extends Model {
+export default class BankAccount extends Model {
+  @PrimaryKey
+  @Default(DataType.UUIDV4)
+  @Column(DataType.UUID)
+  id: string;
 
-    @PrimaryKey
-    @Default(DataType.UUIDV4)
-    @Column(DataType.UUID)
-    id: string;
+  @AllowNull(false)
+  @Default(0)
+  @Column(DataType.FLOAT)
+  balance: number;
 
-    @AllowNull(false)
-    @Column(DataType.STRING)
-    name: string;
+  @ForeignKey(() => User)
+  @AllowNull(false)
+  @Column(DataType.UUID)
+  userId: string;
 
-    @AllowNull(false)
-    @Column(DataType.STRING)
-    userName: string;
-
-    @AllowNull(false)
-    @Column(DataType.STRING)
-    password: string;
-
-    @AllowNull(false)
-    @Column(DataType.STRING)
-    email: string;
-
-    @AllowNull(false)
-    @Default("user")
-    @Column(DataType.STRING)
-    role: string; // 'admin' או 'user'
+  @BelongsTo(() => User)
+  user: User;
 }
